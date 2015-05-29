ca8f2c28a30d9218cfc7ae7f1434c014e716653f
msomji <maisam.somji@gmail.com>
2015-05-29 15:52:39 -0400

    added user stories

---
+++ bowl.rb
@@ -0,0 +1,10 @@
+# as a bowler I want to be able to start a new game
+# as a bowler I want to be able to bowl
+# as a bowler I want to be able to be able to see how many pins I knocked out after each bowl
+# as a bowler I want to be able to see if I get a spare or a strike
+# as a bowler I want to be able to see my score after every frame
+# as a bowloer I would like to get some instructions
+#as a bowler I would want to be able to mannuall enter how many pins I knock out
+
+# features
+# as a bowler I want to have a name?
