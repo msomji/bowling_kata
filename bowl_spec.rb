7f2e9e0d39d3fcd1dc9b22c02a85135fe5415b21
msomji <maisam.somji@gmail.com>
2015-05-29 17:49:31 -0400

    created let block for new instance of game

--- bowl_spec.rb
+++ bowl_spec.rb
@@ -1,31 +1,31 @@
 require_relative 'bowl'
 
 describe "Bowl" do
-
+  let(:game) { Bowl.new}
   it "should let you create a new game" do
-    expect(Bowl.new).to be_a Bowl
+    expect(game).to be_a Bowl
   end
 
   it "should have a score of 0 when a new game is created" do
-    expect(Bowl.new.score).to eq(0)
+    expect(game.score).to eq(0)
   end
 
   it "Should start the game at frame 0" do
-    expect(Bowl.new.frame).to eq(0)
+    expect(game.frame).to eq(0)
   end
 
   it "Should have an empty score board at creation" do
-    expect(Bowl.new.score_board).to eq([])
+    expect(game.score_board).to eq([])
   end
 
   describe "#bowl" do
 
     it "should must accept two integers as aguments for number of pins dropped" do
-      expect{ Bowl.new.bowl(2) }.to raise_error(ArgumentError)
+      expect{ game.bowl(2) }.to raise_error(ArgumentError)
     end
 
     it "should accept 2 integers that are both positive and add up to less than or equal to 10" do
-      expect(Bowl.new.bowl(2,12)).to eq("try again! you can knock out a max of only 10 pins per turn")
+      expect(game.bowl(2,12)).to eq("try again! you can knock out a max of only 10 pins per turn")
     end
   end
 
